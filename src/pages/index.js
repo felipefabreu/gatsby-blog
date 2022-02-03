310850638642d5f9aa3d83e3319433c5bbb446e3
Felipe <felipefabreu@gmail.com>
2022-02-02 23:17:19 -0300

    ajustando estilo

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,14 @@ import styled from 'styled-components';
 
 import { graphql, useStaticQuery } from 'gatsby';
 
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
 const Header = styled.div`
   display: flex;
   align-items: center;
@@ -29,14 +37,14 @@ const Layout = ({ pageTitle, children }) => {
   console.log(data);
 
   return (
-    <>
+    <Body>
       <Header>
         <h1>bebezin</h1>
       </Header>
       <Content>
         <img src="https://t.ctcdn.com.br/l_4EIHeXGXeZ1E1Glfjn3uY3SLc=/i372972.gif"></img>
       </Content>
-    </>
+    </Body>
   );
 };
 
